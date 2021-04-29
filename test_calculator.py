2fe61c2d00e2e0eb1f12b23e978381b2971d4284
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-29 16:46:06 -0700

    added division test cases

--- test_calculator.py
+++ test_calculator.py
@@ -35,5 +35,19 @@ class TestCaseCalculator(unittest.TestCase):
     def test_multiply_exceptions(self):
         self.assertRaises(TypeError, calculator.multiply, 'test', 4)
 
+    # DIVIDE
+    def test_divide_good(self):
+        self.assertEqual(calculator.divide(-20, 4), -5)
+        self.assertAlmostEqual(calculator.divide(3, 4), 0.75)
+    
+    def test_divide_fail(self):
+        self.assertEqual(calculator.divide(3, 4), -1235365)
+    
+    def test_divide_exceptions(self):
+        self.assertRaises(TypeError, calculator.divide, 'test', 4)
+    
+    def test_divide_by_zero(self):
+        self.assertRaises(ZeroDivisionError, calculator.divide, 5, 0)
+
 if __name__ == '__main__':
     unittest.main()
