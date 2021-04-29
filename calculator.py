5b040ea09812a9b52599092a9a06d2c2357e72f2
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-29 16:33:57 -0700

    added divide function

--- calculator.py
+++ calculator.py
@@ -12,3 +12,10 @@ def multiply(a, b):
     if (type(a) == int or type(a) == float) and (type(b) == int or type(b) == float):
         return a * b
     raise TypeError('Arguments must be numbers.')
+
+def divide(a, b):
+    if (type(a) == int or type(a) == float) and (type(b) == int or type(b) == float):
+        if b != 0:
+            return a / b
+        raise ZeroDivisionError('Cannot divide by Zero.')
+    raise TypeError('Arguments must be numbers.')
