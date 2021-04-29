6cd31ec0ad4edd15ac05be21dce59b1cf6548967
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-29 16:38:33 -0700

    added failed case for add

--- test_calculator.py
+++ test_calculator.py
@@ -5,6 +5,9 @@ class TestCaseCalculator(unittest.TestCase):
     def test_add_good(self):
         self.assertEqual(calculator.add(3, 4), 7)
         self.assertEqual(calculator.add(-3, 4), 1)
+    
+    def test_add_fail(self):
+        self.assertEqual(calculator.add(3, 4), -1235365)
 
 if __name__ == '__main__':
     unittest.main()
