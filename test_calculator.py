19140fa271357f3d53d52cb3621141e629a2d197
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-29 16:41:54 -0700

    added test cases for subtract

--- test_calculator.py
+++ test_calculator.py
@@ -2,6 +2,7 @@ import unittest
 import calculator
 
 class TestCaseCalculator(unittest.TestCase):
+    # ADD
     def test_add_good(self):
         self.assertEqual(calculator.add(3, 4), 7)
         self.assertEqual(calculator.add(-3, 4), 1)
@@ -11,6 +12,17 @@ class TestCaseCalculator(unittest.TestCase):
     
     def test_add_exceptions(self):
         self.assertRaises(TypeError, calculator.add, 'test', 4)
+    
+    # SUBTRACT
+    def test_subtract_good(self):
+        self.assertEqual(calculator.subtract(-3, 4), -7)
+        self.assertEqual(calculator.subtract(7, 4), 3)
+    
+    def test_subtract_fail(self):
+        self.assertEqual(calculator.subtract(3, 4), -1235365)
+    
+    def test_subtract_exceptions(self):
+        self.assertRaises(TypeError, calculator.subtract, 'test', 4)
 
 if __name__ == '__main__':
     unittest.main()
