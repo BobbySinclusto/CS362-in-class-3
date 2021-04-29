f2041c2f001b3d1a2ad8b157ac9a93b5a2afe995
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-29 16:30:19 -0700

    added subtract function

--- calculator.py
+++ calculator.py
@@ -2,3 +2,8 @@ def add(a, b):
     if (type(a) == int or type(a) == float) and (type(b) == int or type(b) == float):
         return a + b
     raise TypeError('Arguments must be numbers.')
+
+def subtract(a, b):
+    if (type(a) == int or type(a) == float) and (type(b) == int or type(b) == float):
+        return a - b
+    raise TypeError('Arguments must be numbers.')
