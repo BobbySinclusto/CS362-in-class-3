be73a5eb090e2395303eb72d32f9d6629099cfa7
BobbySinclusto <allencbenjamin@gmail.com>
2021-04-29 16:55:58 -0700

    added user input

--- calculator.py
+++ calculator.py
@@ -19,3 +19,27 @@ def divide(a, b):
             return a / b
         raise ZeroDivisionError('Cannot divide by Zero.')
     raise TypeError('Arguments must be numbers.')
+
+if __name__ == '__main__':
+    print('Welcome to the calculator! Please choose an option.')
+    while (True):
+        print(
+'''
+Choose an option:
+  1. Add
+  2. Subtract
+  3. Multiply
+  4. Divide
+  5. Quit''')
+        choice = input()
+
+        if choice == '1':
+            print(f'Result: {add(eval(input("First number: ")) , eval(input("Second number: ")))}')
+        elif choice == '2':
+            print(f'Result: {subtract(eval(input("First number: ")) , eval(input("Second number: ")))}')
+        elif choice == '3':
+            print(f'Result: {multiply(eval(input("First number: ")) , eval(input("Second number: ")))}')
+        elif choice == '4':
+            print(f'Result: {divide(eval(input("First number: ")) , eval(input("Second number: ")))}')
+        else:
+            break
